1afd4558f30ccebffd8368a6e4e1f3d26cf62060
dwyfrequency <jdwy215@gmail.com>
2018-08-29 22:40:09 -0400

    adding constructor and state in square component

--- src/index.js
+++ src/index.js
@@ -3,14 +3,24 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button className="square" onClick={() => alert("clicked")}>
+        {this.props.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {

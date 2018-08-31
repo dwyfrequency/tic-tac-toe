4d78a5c972dd2274f4fdffb557ced8ef73136c32
dwyfrequency <jdwy215@gmail.com>
2018-08-31 05:32:42 -0400

    adding check for winning game or clicking already used square

--- src/index.js
+++ src/index.js
@@ -19,10 +19,38 @@ class Board extends React.Component {
     };
   }
 
+  calculateWinner = squares => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
   upNext = () => (this.state.xIsNext ? "X" : "O");
 
   handleClick = i => {
     const squares = [...this.state.squares];
+    // if they have already won or the square clicked on is null, return
+    if (this.calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.upNext();
     this.setState({
       squares: squares,
@@ -40,8 +68,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: ${this.upNext()}`;
-
+    const winner = this.calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + this.upNext();
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -73,7 +106,7 @@ class Game extends React.Component {
           <Board />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{/*status */}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>

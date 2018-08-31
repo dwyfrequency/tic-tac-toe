7c8f33843d012aef649b6d84800d5845d35e05b9
dwyfrequency <jdwy215@gmail.com>
2018-08-31 07:25:51 -0400

    adding some basic time traveling

--- src/index.js
+++ src/index.js
@@ -105,6 +105,15 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ? `Go to move #${move}` : `Go to game start`;
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
       status = `Winner ${winner}`;
@@ -123,7 +132,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );

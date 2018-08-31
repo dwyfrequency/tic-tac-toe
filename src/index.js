8a0f3f599a962756d738e9b8bfce91cd84b52ce2
dwyfrequency <jdwy215@gmail.com>
2018-08-31 05:46:44 -0400

    making calc winner global func and changing references that called it

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const calculateWinner = squares => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
 const Square = props => {
   return (
     <button className="square" onClick={props.onClick}>
@@ -19,36 +39,12 @@ class Board extends React.Component {
     };
   }
 
-  calculateWinner = squares => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  };
-
   upNext = () => (this.state.xIsNext ? "X" : "O");
 
   handleClick = i => {
     const squares = [...this.state.squares];
     // if they have already won or the square clicked on is null, return
-    if (this.calculateWinner(squares) || squares[i]) {
+    if (calculateWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = this.upNext();
@@ -68,7 +64,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const winner = this.calculateWinner(this.state.squares);
+    const winner = calculateWinner(this.state.squares);
     let status;
     if (winner) {
       status = "Winner: " + winner;
@@ -99,6 +95,17 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
+      xIsNext: true
+    };
+  }
   render() {
     return (
       <div className="game">
